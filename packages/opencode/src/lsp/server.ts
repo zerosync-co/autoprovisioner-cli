a1cc8f6cdbfd6bde5bc5f0c6d909f07ce4952f4b
Dax Raad <d@ironbay.co>
2025-06-08 12:57:33 -0400

    fix ts lsp

--- packages/opencode/src/lsp/server.ts
+++ packages/opencode/src/lsp/server.ts
@@ -32,8 +32,10 @@ export namespace LSPServer {
           process.argv0 !== "bun"
             ? path.resolve(process.cwd(), process.argv0)
             : process.argv0
-        return spawn(root + " x typescript-language-server --stdio", {
-          argv0: "bun",
+        return spawn(root, ["x", "typescript-language-server", "--stdio"], {
+          env: {
+            BUN_BE_BUN: "1",
+          },
         })
       },
     },
