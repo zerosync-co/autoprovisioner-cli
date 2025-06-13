73e9fb53d525607974758d65b5e460a5dd9d64e2
Dax Raad <d@ironbay.co>
2025-06-13 00:06:15 -0400

    sync

--- packages/opencode/src/cli/cmd/scrap.ts
+++ packages/opencode/src/cli/cmd/scrap.ts
@@ -1,4 +1,5 @@
 import { App } from "../../app/app"
+import { LSP } from "../../lsp"
 import { VERSION } from "../version"
 import { cmd } from "./cmd"
 
@@ -6,9 +7,10 @@ export const ScrapCommand = cmd({
   command: "scrap <file>",
   builder: (yargs) =>
     yargs.positional("file", { type: "string", demandOption: true }),
-  async handler() {
+  async handler(args) {
     await App.provide({ cwd: process.cwd(), version: VERSION }, async (app) => {
-      Bun.resolveSync("typescript/lib/tsserver.js", app.path.cwd)
+      await LSP.touchFile(args.file, true)
+      console.log(await LSP.diagnostics())
     })
   },
 })
