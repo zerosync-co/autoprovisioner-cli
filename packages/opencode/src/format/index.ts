334161a30ecbcf01f164cb267c891db2abd3b612
Ryan Winchester <ryan@winchester.dev>
2025-06-27 11:15:11 -0300

    feat: add elixir file formatting (#458)

--- packages/opencode/src/format/index.ts
+++ packages/opencode/src/format/index.ts
@@ -132,5 +132,32 @@ export namespace Format {
         }
       },
     },
+    {
+      name: "mix format",
+      command: ["mix", "format", "$FILE"],
+      extensions: [
+        ".ex",
+        ".exs",
+        ".eex",
+        ".heex",
+        ".leex",
+        ".neex",
+        ".sface",
+      ],
+      async enabled() {
+        try {
+          const proc = Bun.spawn({
+            cmd: ["mix", "--version"],
+            cwd: App.info().path.cwd,
+            stdout: "ignore",
+            stderr: "ignore",
+          })
+          const exit = await proc.exited
+          return exit === 0
+        } catch {
+          return false
+        }
+      },
+    },
   ]
 }
