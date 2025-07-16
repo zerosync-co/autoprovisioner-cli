c3c647a21a4a04cb80dd90cbc0d78b180584e096
Frank <frank@sst.dev>
2025-07-16 16:20:06 +0800

    wip: github actions

--- scripts/publish-github-action.ts
+++ scripts/publish-github-action.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env bun
 
-import { $, ShellError } from "bun"
+import { $ } from "bun"
 
 try {
   await $`git tag -d github-v1`
   await $`git push origin :refs/tags/github-v1`
-} catch (e) {
-  if (e instanceof ShellError && e.stderr.toString().match(/tag \S+ not found/)) {
+} catch (e: any) {
+  if (e instanceof $.ShellError && e.stderr.toString().match(/tag \S+ not found/)) {
     console.log("tag not found, continuing...")
   } else {
     throw e
